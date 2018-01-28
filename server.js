97b39aa449d84ef7cc23c2ad2ef7a67fab2873dd
Rocky Gray <rocky.grayjr@gmail.com>
2018-01-28 18:24:52 -0500

    add dev proxy for react app

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use('/users', users);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(`${__dirname}/build/index.html`));
 });
 
 // catch 404 and forward to error handler
