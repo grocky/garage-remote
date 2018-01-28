2b16ebfac591eae25b9c4c6484780a31d5867d2e
Rocky Gray <rocky.grayjr@gmail.com>
2018-01-28 16:45:18 -0500

    extract api routes

--- server.js
+++ server.js
@@ -1,36 +1,12 @@
 const Koa = require('koa');
-const Router = require('koa-router');
 const bodyParser = require('koa-body')();
 
 const app = new Koa();
-const router = new Router();
-
-const users = [
-    {
-        name: 'Rocky',
-        email: 'rocky@gmail.com'
-    },
-    {
-        name: 'Foo',
-        email: 'foo@gmail.com'
-
-    },
-    {
-        name: 'Bar',
-        email: 'bar@gmail.com'
-
-    },
-    {
-        name: 'Hot',
-        email: 'hot@gmail.com'
-    }
-];
-
-router.get('/users', ctx => ctx.body = users);
+const api = require('./api');
 
 app
     .use(bodyParser)
-    .use(router.routes())
-    .use(router.allowedMethods())
+    .use(api.routes())
+    .use(api.allowedMethods())
 
 app.listen(process.env.PORT || 3001);
