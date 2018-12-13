990b0d21d9bd148f9ca802def63d9980a4711764
Rocky Gray <rocky.grayjr@gmail.com>
2018-12-13 04:48:51 -0500

    Only publish action if we are already closed or opening
    This prevents publishing for transitioning states.

--- server/routes/garage.js
+++ server/routes/garage.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const log = require('../logger')(module);
 
 router.get('/', function(req, res, next) {
-  req.app.mqtt.publish('garage/open', 'true');
-  res.send(202);
+  const { status } = req.app.state;
+  if (status === 'closed' || status === 'open') {
+    const subTopic = status === 'closed' ? 'open' : 'close';
+    const topic = `garage/${subTopic}`;
+    log('Publishing action', { topic });
+    req.app.mqtt.publish(topic, 'true');
+  }
+  res.sendStatus(202);
 });
 
 module.exports = router;
